e9b458697bbcd588bd97550c5c5c753eab8c287d
reubenprasad <reubenprasad@gmail.com>
2019-08-21 20:33:44 +0530

    heroku

--- libserver.js
+++ libserver.js
@@ -3,32 +3,28 @@ var express = require('express');
 const app = express();
 var authrouter = require("./routes/authrouter")
 var bookrouter = require("./routes/bookrouter")
-var userrouter = require("./routes/userrouter")
-var bodyparser = require('body-parser')
-/* var mongoose = require('mongoose')
-var url = "mongodb://localhost/Library"
-var users = require("./model/User"); 
-mongoose.connect(url,function(err){
-    if(err) 
-    throw err;
-    else
-    console.log("database connected")
-}); */
 const path = require('path')
 app.set("view engine","ejs");
 
 app.use(express.static(path.join(__dirname,"/public")))
 app.use("/book",bookrouter);
 app.use("/auth",authrouter);
-app.use("/user", userrouter)
-app.use(bodyparser.urlencoded({extended:true}))
+
 app.get("/",function(req,res){
-   res.redirect('/user')
+    res.render("index",{pTitle:"Library",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"}]});
     });
 
-app.get("/index",function(req,res){
-    res.render("index",{pTitle:"Library",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"},{link:"/",title:"Log Out"},{link:"/book/addbook",title:"Add New Book"}]});
-    });
+/* app.get("/book",function(req,res){
+    res.render("books",{pTitle:"Books",nav:[{link:"/book",title:"Book"},{link:"/author",title:"Author"}],books:book_arr});
+})
+app.get("/sp/:id",function(req,res){
+   res.render("bookdesc",{pTitle:"Books",nav:[{link:"/book",title:"Book"},{link:"/author",title:"Author"}],books:book_arr[req.params.id]});
+})
+app.get("/auth",function(req,res){
+    res.render("books",{pTitle:"Books",nav:[{link:"/book",title:"Book"},{link:"/author",title:"Author"}],books:book_arr});
+}) */
 
-app.listen(process.env.PORT || 3000, () => console.log('Server Running on http://localhost:3000')); 
-   
+    /* app.listen(2224, function(req,res){
+        console.log("Server started listening")
+    }) */
+    app.listen(process.env.PORT || 3000, () => console.log('Server Running on http://localhost:3000')); 
--- routes/bookrouter.js
+++ routes/bookrouter.js
@@ -1,109 +1,29 @@
 var express = require('express')
 const router = express.Router();
-const path = require('path');
-var multer = require('multer');
-var storage =   multer.diskStorage({  
-    destination: (req, file, callback)=>{  
-      callback(null, './public/images');  
-    },  
-    filename: (req, file, callback)=>{  
-      callback(null, file.originalname);  
-    }  
-  });  
-  var upload = multer({ storage : storage}).single('image');
-var mongoose = require('mongoose')
-var url = "mongodb://localhost/Library"
-var books = require("../model/Book"); 
-mongoose.connect(url,function(err){
-    if(err) 
-    throw err;
-    else
-    console.log("database connected")
-});
+var book_arr = [
+    {
+       title:"City of Joy",
+       genre:"Fiction",
+       author:"Dominique Lapierre",
+       image:"http://t3.gstatic.com/images?q=tbn:ANd9GcR4s3klXwwLcHnXyc-ymOo8ZjdbwFJt9z0H7qm-v4CiRZLlzLr-"     
+    },
+    {
+        title:"Under the Dome",
+        genre:"Fiction",
+        author:"Stephen King",
+        image:"https://images-na.ssl-images-amazon.com/images/I/41PZik3LNWL._SX341_BO1,204,203,200_.jpg"
+    },
+    {
+        title:"White Fang",
+        genre:"Fiction",
+        author:"Jack London",
+        image:"https://images-na.ssl-images-amazon.com/images/I/41cnA1XZPmL.jpg"   
+    }
+]   
 module.exports = router;
 router.get("/",function(req,res){
-    books.find({},function(err,result){
-        
-        res.render("books",{pTitle:"Books",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"},{link:"/book/updatebook", title:"Update Books"},{link:"/book/addbook",title:"Add New Book"},{link:"/",title:"Log Out"}],books:result});
-    })
+    res.render("books",{pTitle:"Books",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"}],books:book_arr});
 })
-router.get("/sp/:title",function(req,res){
-    console.log(req.params.title)
-    books.find({title:req.params.title},function(err,result){
-       
-
-        res.render("bookdesc",{pTitle:"Books",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"},{link:"/book/updatebook", title:"Update Books"},{link:"/book/addbook",title:"Add New Book"},{link:"/",title:"Log Out"}],books:result});    })
-        
+router.get("/sp/:id",function(req,res){
+    res.render("bookdesc",{pTitle:"Books",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"}],books:book_arr[req.params.id]});
  })
- router.get("/addbook",function(req,res){
-    res.render("addbook",{pTitle:"Add New Book",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"},{link:"/book/updatebook", title:"Update Books"},{link:"/book/addbook",title:"Add New Book"},{link:"/",title:"Log Out"}]});
-    });
-
- router.post("/addbook",upload, function(req,res){
-     var b = new books();
-     b.title = req.body.title;
-     b.author = req.body.author;
-     b.genre = req.body.genre;
-     b.image = req.file.filename;
-     b.save(function(err){
-        if (err) throw err;
-        else{
-            console.log("Added");
-            res.redirect("/");
-        }
-    })
-    })
-
-router.get("/view/:img",function(req,res){    
-        res.sendFile(path.join(__dirname+"../../public/images/"+req.params.img))
-})
-
-router.get("/updatebook",function(req,res){
-        books.find({},function(err,result){
-            if (err) throw err;
-            else
-                res.render("updatebook",{pTitle:"Library",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"},{link:"/book/updatebook", title:"Update Books"},{link:"/book/addbook",title:"Add New Book"},{link:"/",title:"Log Out"}],books:result}
-                );
-        }) 
-})
-    
-router.get("/updatebook/:id",function(req,res){
-        books.find({title:req.params.id},function(err,result){
-            if (err) throw err;
-            else res.render("editbook",{pTitle:"Library",nav:[{link:"/book",title:"Book"},{link:"/auth",title:"Author"},{link:"/book/updatebook", title:"Update Books"},{link:"/book/addbook",title:"Add New Book"},{link:"/",title:"Log Out"}],books:result});
-        })
-})
-    
-router.post("/editbook", upload, function(req,res){
-        books.updateOne({title:req.body.title} ,{$set:{
-            title:req.body.title,
-            author : req.body.author,
-            genre : req.body.genre,
-            image : req.file.filename
-        }}, function(err,result){
-            if (err) throw err;
-            else{
-                books.find({},(err,result)=>{
-                    if (err) throw err;
-                    else
-                        res.redirect("/book/updatebook")
-                })
-            }
-        }) 
-    })
-    
-    router.get("/deletebook/:bid",function(req,res){
-        books.deleteOne({title:req.params.bid},function(err,result){
-            if (err) throw err;
-            else
-            {
-                books.find({},(err,result)=>{
-                    if(err) throw err;
-                    else
-                        res.redirect("/book/updatebook")
-                })
-            }
-        })
-    })
-
-
